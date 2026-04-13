4b0a7c0518e213ba4e820aafbd637c5541150563
K-KY <kyuyoungk@naver.com>
2026-04-13 23:31:20 +0900

    feat: 전역 예외 핸들러와 커스텀 예외
    - 요청받은 디렉토리를 찾지 못한경우
    - zip 파일 스트리밍중 예외 핸들러(쓰고있진 않음)

---
+++ src/main/java/com/example/download/exception/ZipStreamingException.java
@@ -0,0 +1,8 @@
+package com.example.download.exception;
+
+public class ZipStreamingException extends RuntimeException {
+
+    public ZipStreamingException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}

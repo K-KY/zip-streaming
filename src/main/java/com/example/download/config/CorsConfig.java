71c3e517eeb0102cad53039a8f61eda2bb9b35dd
K-KY <kyuyoungk@naver.com>
2026-04-18 21:29:09 +0900

    feat: cors 설정 추가

---
+++ src/main/java/com/example/download/config/CorsConfig.java
@@ -0,0 +1,21 @@
+package com.example.download.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    private final String[] allowedOrigins = {
+           "http://localhost:5173"
+    };
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(allowedOrigins)
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
